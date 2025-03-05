95be0dcf93f0131611a9d612401d4d014c97dfc4
FINCONSGROUP\matteo.proietti <matteo.proietti@outlook.com>
2025-03-05 09:46:55 +0100

    FE

--- obj/Debug/net8.0/GestioneAccounts.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestioneAccounts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestioneAccounts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e455bbf1fb4307fa3247345fc56df0b0e61ab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestioneAccounts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestioneAccounts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
